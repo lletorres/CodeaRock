5728b1e83d998f41dccdd5b0ee070228dc15cc4b
lletorres <83483730+lletorres@users.noreply.github.com>
2024-01-28 19:07:23 -0300

    Update Reto02.js

--- Reto02.js
+++ Reto02.js
@@ -1,5 +1,11 @@
-let saludo = document.getElementById("saludo");
-let nombre = document.getElementById("nombre").value;
-console.log("lo que tiene saludo", saludo.innerHTML);
-console.log("lo que tiene nombre", nombre);
-saludo.innerHTML += nombre
+const saludito = () => {
+  let saludo = document.getElementById("saludo");
+  let nombre = document.getElementById("nombre").value; // que pasa si se guarda afuera?
+  // los input tienen un atributo VALUE que nos da el valor de su contenido, como el innerHTML pero para inputs
+  // se le agrega .VALUE porque es lo que tiene el input de NOMBRE
+  console.log("lo que tiene nombre", nombre);
+  //cuando se pone VALUE  se guarda el valor de la etiqueta adentro de la variable NOMBRE, no se ponde innerHTML
+  console.log("lo que tiene saludo", saludo.innerHTML);
+
+  saludo.innerHTML = "Bienvenido" + nombre;
+};
